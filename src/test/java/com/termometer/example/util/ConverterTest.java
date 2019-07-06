194fb3a17ca890931dd863876c76a212e844731c
Carlos <carlosraygoza@gmail.com>
2019-07-06 13:01:32 -0700

    sample

--- src/test/java/com/termometer/example/util/ConverterTest.java
+++ src/test/java/com/termometer/example/util/ConverterTest.java
@@ -19,7 +19,7 @@ public class ConverterTest {
 
     @Test
     public void shouldReturnFarenheitWhenCelciusIsInput() {
-        double expectedFarenheitResult = 75;
+        double expectedFarenheitResult = 78;
         double celsiusInput = 23.88;
 
         assertEquals(expectedFarenheitResult,
